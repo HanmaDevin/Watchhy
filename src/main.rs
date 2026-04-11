88a84bdd9f477687c139068d8b65f05e21f2df51
HanmaDevin <Hanma@Devin.com>
2026-04-11 17:05:22 +0200

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,38 @@
 use gtk::{
-    ApplicationWindow,
-    gio::prelude::{ApplicationExt, ApplicationExtManual},
+    ApplicationWindow, Window,
+    gio::{
+        self,
+        prelude::{ApplicationExt, ApplicationExtManual},
+    },
     glib::{self},
     prelude::GtkWindowExt,
 };
 
 use crate::{
+    config::app_id,
     models::Mode,
     scraper::{get_episode_streams, get_video_url_with_quality, search_anime},
 };
 
+mod config;
 mod models;
 mod scraper;
-
-pub const APP_ID: &str = "org.hanmadevin.Watchhy";
+mod window;
 
 fn main() -> glib::ExitCode {
-    let app = adw::Application::builder().application_id(APP_ID).build();
+    let res = gio::Resource::load("../resources/resources.gresource.xml")
+        .expect("Could not load gresource file");
+    gio::resources_register(&res);
+
+    let app = adw::Application::builder().application_id(app_id()).build();
 
     app.connect_activate(build_ui);
-
-    // test code
-    let mode = Mode::Sub;
-    let search = search_anime("liar game", mode.clone());
-    println!("{search:#?}");
-    let streams = get_episode_streams(search[0].0.as_str(), mode, "1");
-    println!("{streams:#?}");
-    let url = get_video_url_with_quality(models::Quality::HD1080, streams[0].1.as_str());
-    println!("{url}");
 
     app.run()
 }
 
 fn build_ui(app: &adw::Application) {
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("Watchhy")
-        .build();
+    let window = Window::new(app);
 
     window.present();
 }
